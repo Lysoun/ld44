dcafb59f4991da0af278f9ec16e95ece4238aeb7
Alexandre Solovieff <asolovieff@hotmail.fr>
2019-04-27 20:09:36 +0200

    Xp + Age + Level Up update
    Add the statistic xp and xpMax. xpMax corresponds to the xp needded to level up.
    Aging of the cards.
    Update of the state when level up.

--- Assets/Scripts/CardData.cs
+++ Assets/Scripts/CardData.cs
@@ -15,9 +15,23 @@ public class CardData : ScriptableObject
     public int armorMax;
     public int speedMin;
     public int speedMax;
+    public int ageMin;
+    public int ageMax;
 
     public int cost;
 
     public Texture artwork;
 
+    public int xpMax;
+    private int xp = 0;
+
+    
+    /// <summary>
+    /// Return the amount of xp of the card.
+    /// </summary>
+    /// <returns></returns>
+    public int getXp()
+    {
+        return xp;
+    }
 }
