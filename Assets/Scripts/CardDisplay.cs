2005773a2cb1e602826bd967af4b9127bba2cae2
Alexandre Solovieff <asolovieff@hotmail.fr>
2019-04-27 12:15:21 +0200

    Card (Start)
    Scriptable Object of  the cards.
    Basic prefab of a card.

---
+++ Assets/Scripts/CardDisplay.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CardDisplay : MonoBehaviour
+{
+    public CardData card;
+
+    public TextMeshProUGUI typeText;
+
+    public TextMeshProUGUI healthText;
+    public TextMeshProUGUI atkText;
+    public TextMeshProUGUI armorText;
+
+    public TextMeshProUGUI costText;
+
+    public RawImage artwork;
+
+    // Initialize the content of the card prefab.
+    void Start()
+    {
+        System.Random rnd = new System.Random();
+
+        typeText.SetText(card.type);
+
+        healthText.SetText((card.health).ToString());
+        atkText.SetText((rnd.Next(card.atkMin, card.atkMax)).ToString());
+        armorText.SetText((rnd.Next(card.armorMin, card.armorMax)).ToString());
+
+        costText.SetText((card.cost).ToString());
+
+        artwork.texture = card.artwork;
+    }
+
+}
